ba4515270b693f1612b19d8899ebc944191020c8
egor <rank2007@mail.ru>
2022-07-11 21:16:02 +0300

    remove unused imports

--- Anti-Fraud System/task/src/antifraud/Service/CardService.java
+++ Anti-Fraud System/task/src/antifraud/Service/CardService.java
@@ -7,13 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class CardService {
